441853591ece372bb0cd80773f8040cf706e7fee
kcarvin43 <57299777+kcarvin43@users.noreply.github.com>
2019-11-02 21:31:55 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+def hello_world():
+   print('Hello World')
+
+hello_world()
+
+"""
+    Very easy program, literally the output is going to be Hello World
+    Wanted to get some practice defining a function
+    To call your function, list your function name followed by the two
+    parentheses "()" like shown in line 4
+"""
